41309d8bfb0fd312de173648acea04b019db2fae
Rune Lien <lien.runelien@gmail.com>
2011-06-30 07:24:29 +0200

    Episode 2

--- test/blueprint_test.js
+++ test/blueprint_test.js
@@ -1,6 +1,30 @@
-TestCase("TestSomething", sinon.testCase({
+(function(blueprint){
+	TestCase("TestBlueprint", sinon.testCase({
 
-       "test should do stuff": function (stub, mock) {
-          assert(true);      
-        }
-}));
+	       "test should be an object": function () {
+	          assertObject(blueprint);      
+	        },
+
+			"test should create blueprints": function () {
+				var bp = blueprint.create([]);
+				assertProtoTypeOf(blueprint,bp);
+			},
+			
+			"test should have shape": function (stub, mock) {
+			    var shape = ["***",
+							 "***"];
+			    var bp = blueprint.create(shape);
+				assertEquals(shape,bp.shape);
+			},
+			 
+			"test should complain if shape is missing": function (stub, mock) {
+			   assertException(function(){
+					blueprint.create();
+				});     
+			}
+			
+			
+
+	}));	
+}(ZOMBIE.blueprint));
+
